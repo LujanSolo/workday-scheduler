3dfc3bbda658c2e94f85962e0d89665088b7b7be
LujanSolo <lujansolo@proton.me>
2022-09-04 23:13:36 -0700

    added setItem to local storage function tied to each save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
+//*establishing globally scoped variables:
+let saveBtn = $('.save-btn');
+
+//* Begin with a document.ready function to load the current day to the top of the page with moment.js...
 $(document).ready(function displayDay() {
     let currentDay = $('#currentDay');
     let todayIs = moment().format("[Today is] dddd");
     currentDay.text(todayIs);
+});
+
+
+
+
+
+//*When the user clicks save at the end of any hour row, the entry from the input field goes to local storage...
+saveBtn.on('click', function() {
+    let timeBlock = $(this).siblings(".time-block").val();
+    localStorage.setItem("note: ", timeBlock);
 })
